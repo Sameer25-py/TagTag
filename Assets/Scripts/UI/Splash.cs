27ef51ea59be79e38fab072c6385d82af2a1948f
Muhammad Sameer <sameergb25@outlook.com>
2023-05-20 01:47:44 +0500

    implement splash scene

---
+++ Assets/Scripts/UI/Splash.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class Splash : MonoBehaviour
+    {
+        private void Start()
+        {
+            Invoke(nameof(LoadMainMenu), 1.5f);
+        }
+
+        private void LoadMainMenu()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
